73438d232f8893e6eddb11ea0da437608e3c3d01
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-05-16 21:30:02 -0300

    key

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,9 +1,16 @@
-import 'LoginPage.css';
+import './LoginPage.css';
+import { useState } from 'react';
 
 export const LoginPage = () => {
+    const [key, setkey] = useState('');
+
     return(
         <div className="loginPage">
-            
+            <h1>Meu Time</h1>
+                <div>
+                    <h3>Insira de Chave da API-Football</h3>
+                    <input type="text" placeholder='insira sua chave' onChange={(e) => setkey(e.target.value)}/>
+                </div>
         </div>
     );
 }
