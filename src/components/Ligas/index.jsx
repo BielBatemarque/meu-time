49b9f6225ec70194a4731af4c9143c64abf82845
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-05-21 18:52:55 -0300

    requisição componente lgias

--- src/components/Ligas/index.jsx
+++ src/components/Ligas/index.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react';
 import { Menu } from '../Menu/Menu';
 import './Ligas.css';
+import { options } from '../../data/data';
 
 export const Ligas = () => {
+    const[ligas, setLigas] = useState([]);
+
+    useEffect(() =>{
+        fetchApi();
+    }, []);
+
+    const fetchApi = async () => {
+
+        const api = await fetch('https://api-football-v1.p.rapidapi.com/v3/leagues?country=England', options);
+        const json = await api.json();
+        setLigas(json);
+        console.log(ligas);
+    } 
+
     return(
         <div className="Ligas">
             <Menu />
-            
+            <div className='Ligas'>
+                <p>{}</p>
+            </div>
         </div>
     );
 }
