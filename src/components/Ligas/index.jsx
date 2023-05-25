b1ef6cd226e7b1ee469bd322e2eb5f49eea27c21
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-05-25 01:07:42 -0300

    filtrando lista de ligas

--- src/components/Ligas/index.jsx
+++ src/components/Ligas/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Menu } from '../Menu/Menu';
 import './Ligas.css';
 import { options } from '../../data/data';
@@ -6,35 +6,45 @@ import { Loader } from '../Loader';
 import { Input } from '../Input';
 
 export const Ligas = () => {
-    const[ligas, setLigas] = useState({});
+    const[ligas, setLigas] = useState([]);
     const[allLeagues, setAllLeagues] = useState([]);
     const[pages, setAllPages] = useState(0);
-    const[leaguesPerpage] = useState(0);
+    const[leaguesPerpage] = useState(15);
     const[searchValue, setSearchValue] = useState('');
 
-    useEffect(() =>{
-        handleFetch();
+    const handleLoadLeagues = useCallback(async (page, ligasPorPagina) => {
+        const carregaLigas = await handleFetch();
+        
+        setLigas(carregaLigas.slice(page, ligasPorPagina));
+        setAllLeagues(carregaLigas);
     }, []);
+
+    useEffect(() => {
+        handleLoadLeagues(0, leaguesPerpage);
+    }, [handleLoadLeagues, leaguesPerpage]);
     
     const handleFetch = async () => {
         const api = 'https://api-football-v1.p.rapidapi.com/v3/leagues';
         const request = await fetch(api, options);
         const json = await request.json();
-        setLigas(json);
-    } 
+        const { response } = json;
+        console.log(response);
+        return response;
+    };
 
-    const { response } = ligas;
-    console.log(response);
-    
+
+    // const { response } = ligas;
+    // console.log(response);
+    console.log(ligas);
     return(
         <div className="Ligas">
             <Menu />
             <h1 className='mainTitle'>Ligas</h1>
             <Input text={'Pesquisar Liga'} />
             <div className='listaLigas'>
-                {!response ? 
+                {!ligas ? 
                     <Loader text={'carregando'} />
-                : response.map(liga => (
+                : ligas.map(liga => (
                     <div key={liga.league.id} className='card'>
                         <h3>{liga.league.name}</h3>
                         <span className="text">{liga.country.name}</span>
