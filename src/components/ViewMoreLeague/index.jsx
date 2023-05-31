790bda72e4c814cf015b2182e01eb9f9115798ad
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-05-31 16:28:41 -0300

    tratamento id

--- src/components/ViewMoreLeague/index.jsx
+++ src/components/ViewMoreLeague/index.jsx
@@ -3,12 +3,12 @@ import { useParams } from 'react-router-dom';
 
 export const ViewMoreLeague = ({ match }) => {
     const { id } = useParams();
+    let confirmId = id.substring(1);
+    confirmId = Number(confirmId);
 
-
-    console.log(id)
     return(
         <div className="ViewMoreLeague">
-            <h1>{id}</h1>
+            <h1>{confirmId}</h1>
         </div>
     );
 }
