637b7e2cf2dd269e9fc9f9f6b686111c0d936453
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-05-31 14:07:25 -0300

    useParams

--- src/components/ViewMoreLeague/index.jsx
+++ src/components/ViewMoreLeague/index.jsx
@@ -1,6 +1,9 @@
 import './ViewMoreLeague.css';
+import { useParams } from 'react-router-dom';
 
-export const ViewMoreLeague = ({ id }) => {
+export const ViewMoreLeague = ({ match }) => {
+    const { id } = useParams();
+    console.log(id)
     return(
         <div className="ViewMoreLeague">
             <h1>{id}</h1>
