435c164d029643c94f69080c2c43842cc04505f0
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-06-01 15:24:12 -0300

    request liga por id

--- src/components/ViewMoreLeague/index.jsx
+++ src/components/ViewMoreLeague/index.jsx
@@ -1,18 +1,30 @@
+import { useEffect, useState } from 'react';
 import { Menu } from '../Menu/Menu';
 import './ViewMoreLeague.css';
 import { useParams } from 'react-router-dom';
+import { options } from '../../data/data';
 
 export const ViewMoreLeague = ({ match }) => {
     const { id } = useParams();
+    const [inforLigas, setInfoLigas] = useState();
+
     let confirmId = id.substring(1);
     confirmId = Number(confirmId);
 
+    const url = `https://api-football-v1.p.rapidapi.com/v3/leagues?id=${confirmId}`;
+
     const getInfoLeagues = async () => {
-        const request = await fetch('');
+        const request = await fetch(url, options);
         const json = await request.json();
 
-        return(json);
+        setInfoLigas(json);
     }
+
+    useEffect(() => {
+        getInfoLeagues();
+    }, []);
+
+    console.log(inforLigas);
 
     return(
         <div className="ViewMoreLeague">
