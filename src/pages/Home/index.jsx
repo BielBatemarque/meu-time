2206055754d9f45b5581d36c15d3fcdbb7c23f85
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-06-07 01:23:13 -0300

    inicio da pagina home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,14 +10,16 @@ export const Home = () => {
                 <h1>Meu-Time</h1>
             </div>
             <section className='section-home'>
-                <p>sessão de apresentação</p>
-                <Button text={'Lougout'} action={() => console.log('deslogar criando um estado global da função que seta algumas informações como logado')}/>
+               <div className='dh1'></div>
+               <div className='dh1'></div>
+               
             </section>
             <section>
                 <p>sessão sobre apresentação da aplicação</p>
             </section>
             <section>
                 <p>apresenta times e ligas</p>
+                <Button text={'Lougout'} action={() => console.log('deslogar criando um estado global da função que seta algumas informações como logado')}/>
             </section>
         </div>
     );
