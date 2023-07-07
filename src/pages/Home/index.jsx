61198cbb4728bc5becbfd14200b9afcd9bd7b4cc
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-07-07 02:35:36 -0300

    imagem do card de ligas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,45 +1,61 @@
-
 import { Menu } from '../../components/Menu/Menu';
 import { Card } from '../../components/Card/index';
 import campo from '../../assets/campo.png';
 import { globalContext } from '../../context/globalContext';
 import './Home.css';
-import { Footer } from '../../components/Footer';
-import { useEffect, useContext } from 'react';
-export const Home = () => {
-    console.log();
+import React,{ useEffect, useContext, useState } from 'react';
+import { options } from '../../data/data';
+import { Footer } from '../../components/Footer/index';
 
+export const Home = () => {
     const { state } = useContext(globalContext);
+    const [imageLeague, setImageLeague] = useState('');
 
     useEffect(() => {
-        console.log(state);
-    }, [state]);
+        loadLeagueImage();
+    }, []);
 
+    const handleSorteiaNumero = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
 
-    return(
+    const loadLeagueImage = async () => {
+        const request = await fetch('https://api-football-v1.p.rapidapi.com/v3/leagues', options);
+        const response = await request.json();
+        const ligas = await response.response;
+        const liga = ligas[handleSorteiaNumero(0, 1020)];
+        const { league } = liga;
+        const { logo } = league;
+
+        setImageLeague(logo);
+    }
+
+    return (
         <div className="Home">
             <Menu />
             <div className='divTitle'>
                 <h1>Meu-Time</h1>
             </div>
             <section className='section-home'>
-               <div className='dh1'>
+                <div className='dh1'>
                     <h2 className='title-descricao'>Informações</h2>
                     <p className='descricao'>Informações sobre Futebol, fazendo consumo da API publica disponibilizada pela API-Sportsbr <br />
                     Sendo Possivel consultar tudo sobre o mundo do Futebol
                     </p>
 
-               </div>
-               <div className='dh1'>
-                    <img src={campo} alt="campoIMG" className='imgcampo'/>
-               </div>
-               
+                </div>
+                <div className='dh1'>
+                    <img src={campo} alt="campoIMG" className='imgcampo' />
+                </div>
+
             </section>
             <section className='cardList'>
-                <Card title={'Ligas'} />
-                <Card title={'Times'}/>
-                <Card title={'Temporadas'}/>
-                <Card title={'Jogadores'}/>
+                <MemoizedCard title={'Ligas'} logo={imageLeague} />
+                <Card title={'Times'} />
+                <Card title={'Temporadas'} />
+                <Card title={'Jogadores'} />
             </section>
             <section>
                 <p>apresenta times e ligas</p>
@@ -49,3 +65,5 @@ export const Home = () => {
         </div>
     );
 }
+
+const MemoizedCard = React.memo(Card);
